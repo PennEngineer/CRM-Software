a365feddc25fd42960791f0021eef92b8cdb5e9f
PennEngineer <Kevn@seas.upenn.edu>
2020-09-06 16:33:44 -0500

    Able to delete customers from CRM

--- src/springapp/dao/CustomerDAOImpl.java
+++ src/springapp/dao/CustomerDAOImpl.java
@@ -48,4 +48,12 @@ public class CustomerDAOImpl implements CustomerDAO {
 		return currentSession.get(Customer.class, theId);
 	}
 
+	@Override
+	public void deleteCustomer(int theId) {
+		Session currentSession = sessionFactory.getCurrentSession();
+		Query theQuery = currentSession.createQuery("delete from Customer where id=:customerId");
+		theQuery.setParameter("customerId", theId);
+		theQuery.executeUpdate();
+	}
+
 }
